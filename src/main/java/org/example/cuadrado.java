07179dbf502e0ea3eb74be64c8fdd236ff5806cf
JUAN PUENTES <juan_puentesto@fet.edu.co>
2025-09-03 21:59:59 -0500

    ver 2

---
+++ src/main/java/org/example/cuadrado.java
@@ -0,0 +1,22 @@
+    package org.example;
+
+    public class cuadrado {
+        private double lado1;
+        private double lado2;
+
+        public cuadrado() {
+            this.lado1 = 4.5;
+            this.lado2 = 4.5;
+    }
+
+    public double area () {
+        return lado1 * lado2;
+    }
+    public double perimetro() {
+            return 2 * (lado1 + lado2); }
+
+    }
+
+
+
+
